75351066ac6d722568409420196183eda584dc83
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-07-09 13:45:51 +0800

    removed vite fn

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [
-    laravel({
-      input: ["resources/css/app.scss", "resources/js/main.tsx"],
-      refresh: true,
-    }),
-    react(),
-    tsconfigPaths(),
+    // laravel({
+    //   input: ["resources/css/app.css", "resources/js/app.ts"],
+    //   refresh: true,
+    // }),
   ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
 });
