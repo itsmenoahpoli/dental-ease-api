4f26cac1381e23eef6feb4de5b0641fb8c1ba34b
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-07-23 12:06:30 +0800

    init

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{any}', function () {
-    return view('app');
-})->where('any', '^(?!api\/)[\/\w\.-]*');
+// Route::get('{any}', function () {
+//     return view('app');
+// })->where('any', '^(?!api\/)[\/\w\.-]*');
